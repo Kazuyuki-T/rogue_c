528e5b0ab65d72012e516e7a81f89249992b65fc
Kazuyuki-T <s1610110@jaist.ac.jp>
2017-12-01 16:04:59 +0900

    testRule 削除

--- Rule.c
+++ Rule.c
@@ -4,8 +4,6 @@
 void Rule_init(Rule *thisRule) {
 	Rule_setMapSizeX(thisRule, 1000);
 	Rule_setMapSizeY(thisRule, 10);
-	thisRule->testRule = 0;
-	thisRule->testRule = 252;
 }
 
 void Rule_finish(Rule *thisRule) {
